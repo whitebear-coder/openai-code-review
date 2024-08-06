3bd45292293d77f593cde8683469535c1b606204
Zexu Lin <1664289871@qq.com>
2024-08-06 20:37:38 +0800

    v2 OpenAiCodeReviewTest

--- openai-code-review-test/src/test/java/cn/sun/gaga/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/sun/gaga/test/ApiTest.java
@@ -1,9 +1,11 @@
 package cn.sun.gaga.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 import javax.swing.*;
 
