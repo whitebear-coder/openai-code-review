0a1a26d0cdac98c37d2d5b0fd4a602b13f9c204c
Zexu Lin <1664289871@qq.com>
2024-08-06 21:43:44 +0800

    glm code review v2

--- openai-code-review-sdk/src/main/java/cn/sun/gaga/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/sun/gaga/sdk/OpenAiCodeReview.java
@@ -1,5 +1,7 @@
 package cn.sun.gaga.sdk;
+import cn.sun.gaga.sdk.domain.model.ChatCompletionRequest;
 import cn.sun.gaga.sdk.domain.model.ChatCompletionSyncResponse;
+import cn.sun.gaga.sdk.domain.model.Model;
 import cn.sun.gaga.sdk.types.utils.BearerTokenUtils;
 import com.alibaba.fastjson2.JSON;
 
@@ -10,6 +12,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 
 public class OpenAiCodeReview {
@@ -40,7 +43,7 @@ public class OpenAiCodeReview {
         // 2. chatglm 代码评审
 
         String log = codeReview(diffCode.toString());
-        System.out.println("codeReview: "+log);
+        System.out.println("codeReview: "+ log);
     }
 
     private static String codeReview(String diffCode) throws Exception{
@@ -56,20 +59,24 @@ public class OpenAiCodeReview {
         connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
         connection.setDoOutput(true);
 
-        String jsonInpuString = "{"
-                + "\"model\":\"glm-4-flash\","
-                + "\"messages\": ["
-                + "    {"
-                + "        \"role\": \"user\","
-                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + diffCode + "\""
-                + "    }"
-                + "]"
-                + "}";
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
+            private static final long serialVersionUID = -7988151926241837899L;
 
-        try(OutputStream os = connection.getOutputStream()){
-            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            {
+                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user", diffCode));
+            }
+        });
+
+
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input);
         }
+
 
         int responseCode = connection.getResponseCode();
         System.out.println(responseCode);
@@ -85,6 +92,7 @@ public class OpenAiCodeReview {
         in.close();
         connection.disconnect();
 
+        System.out.println("评审结果：" + content.toString());
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
 
