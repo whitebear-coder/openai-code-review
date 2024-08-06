57b4549954ab5a885609863adc70094851c6e7f9
Zexu Lin <1664289871@qq.com>
2024-08-06 20:30:57 +0800

    OpenAiCodeReviewTest

--- openai-code-review-test/src/test/java/cn/sun/gaga/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/sun/gaga/test/ApiTest.java
@@ -14,6 +14,8 @@ public class ApiTest {
 
     @Test
     public void Test(){
-        System.out.println(Integer.parseInt("aaaa"));
+        System.out.println(Integer.parseInt("aaaa1"));
+        System.out.println(Integer.parseInt("aaaa2"));
+        System.out.println(Integer.parseInt("aaaa3"));
     }
 }
