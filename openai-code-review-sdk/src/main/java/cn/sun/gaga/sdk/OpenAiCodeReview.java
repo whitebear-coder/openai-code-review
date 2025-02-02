4c6aab5e4381ce14904309a8ccf51198320491ae
Zexu Lin <1664289871@qq.com>
2025-02-02 15:24:09 +0800

    feat:add local branch

--- openai-code-review-sdk/src/main/java/cn/sun/gaga/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/sun/gaga/sdk/OpenAiCodeReview.java
@@ -33,14 +33,14 @@ public class OpenAiCodeReview {
     private static final Logger logger = LoggerFactory.getLogger(OpenAiCodeReview.class);
 
     // 配置配置
-    private String weixin_appid = "wx3e757551ebe7133e";
-    private String weixin_secret = "8a67bafda92906fe893f149ca428916e";
-    private String weixin_touser = "owICi6cm1pfbDIH45eIl2P-ODwKw";
-    private String weixin_template_id = "pfjekTkVdA9OpL3qli5wmu5doY68A9KvWYjl5U_5jTM";
+    private static final String weixin_appid = "wx3e757551ebe7133e";
+    private static final String weixin_secret = "8a67bafda92906fe893f149ca428916e";
+    private static final String weixin_touser = "owICi6cm1pfbDIH45eIl2P-ODwKw";
+    private static final String weixin_template_id = "pfjekTkVdA9OpL3qli5wmu5doY68A9KvWYjl5U_5jTM";
 
     // ChatGLM 配置
-    private String chatglm_apiHost = "https://open.bigmodel.cn/api/paas/v4/chat/completions";
-    private String chatglm_apiKeySecret = "";
+    private static final String chatglm_apiHost = "https://open.bigmodel.cn/api/paas/v4/chat/completions";
+    private static final String chatglm_apiKeySecret = "";
 
     // Github 配置
     private String github_review_log_uri;
@@ -65,15 +65,15 @@ public class OpenAiCodeReview {
          * 项目：{{repo_name.DATA}} 分支：{{branch_name.DATA}} 作者：{{commit_author.DATA}} 说明：{{commit_message.DATA}}
          */
         WeiXin weiXin = new WeiXin(
-                getEnv("WEIXIN_APPID"),
-                getEnv("WEIXIN_SECRET"),
-                getEnv("WEIXIN_TOUSER"),
-                getEnv("WEIXIN_TEMPLATE_ID")
+                getEnv("WEIXIN_APPID")!=null?getEnv("WEIXIN_APPID"):weixin_appid,
+                getEnv("WEIXIN_SECRET")!=null?getEnv("WEIXIN_SECRET"):weixin_secret,
+                getEnv("WEIXIN_TOUSER")!=null?getEnv("WEIXIN_TOUSER"):weixin_touser,
+                getEnv("WEIXIN_TEMPLATE_ID")!=null?getEnv("WEIXIN_TEMPLATE_ID"):weixin_template_id
         );
 
 
 
-        IOpenAI openAI = new ChatGLM(getEnv("CHATGLM_APIHOST"), getEnv("CHATGLM_APIKEYSECRET"));
+        IOpenAI openAI = new ChatGLM(getEnv("CHATGLM_APIHOST")!=null?getEnv("CHATGLM_APIHOST"):chatglm_apiHost, getEnv("CHATGLM_APIKEYSECRET")!=null?getEnv("CHATGLM_APIKEYSECRET"):chatglm_apiKeySecret);
 
         OpenAiCodeReviewService openAiCodeReviewService = new OpenAiCodeReviewService(gitCommand, openAI, weiXin);
         openAiCodeReviewService.exec();
